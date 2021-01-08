df315c1d7b1f97bee6059b9bab022e7115afff12
oguni <oguni@b-forme.net>
2021-01-08 14:36:47 +0900

    【Seeder】Userに追加するダミーデータを作成

---
+++ config/app.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'timezone' => 'Asia/Tokyo',
+    'locale' => 'ja',
+    //fakerを日本語化
+    'faker_locale' => 'ja_JP',
+];
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,9 +21,26 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        //男女を連想配列で準備
+        $gender = ['male' => 1, 'female' => 2];
+        //ランダムでひとつ、キーを取り出す
+        $genderkey = array_rand($gender, 1);
+        //症例を準備
+        $diagnosis = [
+            '脳性まひ',
+            'てんかん',
+            '水頭症',
+            '脳脊髄腫瘍',
+            '頭頸部腫瘍'
+        ];
+        //nameは、'male', 'female'のどちらかが入り、性別の名前を生成する
+        //nameの性別と連動してsexに数値が入る
         return [
-            'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
+            'name' => $this->faker->name($genderkey),
+            'sex' => $gender[$genderkey],
+            'age' => $this->faker->numberBetween(1, 16),
+            'diagnosis' => $this->faker->randomElement($diagnosis),
+            'note' => $this->faker->realText,
         ];
     }
 }
