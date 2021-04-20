a41cba114bcd27e7a54ff0e681a4506d46071a67
oguni <oguni@b-forme.net>
2021-04-20 11:57:49 +0900

    【Weather】変更（PUT）を追加

--- app/Http/Controllers/WeatherRecordController.php
+++ app/Http/Controllers/WeatherRecordController.php
@@ -9,30 +9,44 @@ use App\Http\Resources\WeatherRecord as WeatherRecordResource;
 
 class WeatherRecordController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index(Request $request)
-    {
-        if ($request->query('day')) {
-            $weather = WeatherRecord::where('day', $request->day)->firstorFail();
-            return new WeatherRecordResource($weather);
-        }
-        $weathers = WeatherRecord::all();
-        return new WeatherRecordCollection($weathers);
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index(Request $request)
+  {
+    if ($request->query('day')) {
+      $weather = WeatherRecord::where('day', $request->day)->firstorFail();
+      return new WeatherRecordResource($weather);
     }
+    $weathers = WeatherRecord::all();
+    return new WeatherRecordCollection($weathers);
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $weather = WeatherRecord::create($request->all());
-        return new WeatherRecordResource($weather);
-    }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    $weather = WeatherRecord::create($request->all());
+    return new WeatherRecordResource($weather);
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
+    $weather = WeatherRecord::findorFail($id);
+    $weather->update($request->all());
+    return new WeatherRecordResource($weather);
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,25 +17,26 @@ use Carbon\Carbon;
 
 
 $router->group(['prefix' => 'api'], function ($router) {
-    //利用者API
-    $router->get('/users', 'UserController@index');
-    $router->post('/users', 'UserController@store');
-    $router->get('/users/{id}', 'UserController@show');
-    $router->put('/users/{id}', 'UserController@update');
-    $router->delete('/users/{id}', 'UserController@destroy');
-    $router->post('/users/selectdelete', 'UserController@selectdelete');
-    //天候API
-    $router->get('/weather_records', 'WeatherRecordController@index');
-    $router->post('/weather_records', 'WeatherRecordController@store');
-    //診断記録API
-    $router->get('/index/medicals', 'MedicalController@simpleIndex');
-    $router->get('/medicals', 'MedicalController@index');
-    $router->post('/medicals', 'MedicalController@store');
-    $router->get('/medicals/{id}', 'MedicalController@show');
-    $router->put('/medicals/{id}', 'MedicalController@update');
-    $router->delete('/medicals/{id}', 'MedicalController@destroy');
+  //利用者API
+  $router->get('/users', 'UserController@index');
+  $router->post('/users', 'UserController@store');
+  $router->get('/users/{id}', 'UserController@show');
+  $router->put('/users/{id}', 'UserController@update');
+  $router->delete('/users/{id}', 'UserController@destroy');
+  $router->post('/users/selectdelete', 'UserController@selectdelete');
+  //天候API
+  $router->get('/weather_records', 'WeatherRecordController@index');
+  $router->post('/weather_records', 'WeatherRecordController@store');
+  $router->put('/weather_records/{id}', 'WeatherRecordController@update');
+  //診断記録API
+  $router->get('/index/medicals', 'MedicalController@simpleIndex');
+  $router->get('/medicals', 'MedicalController@index');
+  $router->post('/medicals', 'MedicalController@store');
+  $router->get('/medicals/{id}', 'MedicalController@show');
+  $router->put('/medicals/{id}', 'MedicalController@update');
+  $router->delete('/medicals/{id}', 'MedicalController@destroy');
 });
 
 Route::get('{path:.*}', function () {
-    return view('app');
+  return view('app');
 });
