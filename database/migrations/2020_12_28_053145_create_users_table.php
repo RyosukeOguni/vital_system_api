dff5dab6cfcf31867468e7dd670d963517b2e251
oguni <oguni@b-forme.net>
2021-04-23 14:42:59 +0900

    migrate設定の修正（性別をintger）

--- database/migrations/2020_12_28_053145_create_users_table.php
+++ database/migrations/2020_12_28_053145_create_users_table.php
@@ -6,33 +6,33 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id')->comment('ID');
-            $table->string('name')->comment('利用者名');
-            $table->integer('age')->comment('年齢');
-            $table->char('sex')->comment('性別');
-            $table->string('diagnosis')->comment('診断名');
-            $table->text('note')->nullable()->comment('備考');
-            $table->softDeletes()->comment('削除日時');
-            $table->timestamp('created_at')->nullable()->comment('登録日時');
-            $table->timestamp('updated_at')->nullable()->comment('更新日時');
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('users', function (Blueprint $table) {
+      $table->bigIncrements('id')->comment('ID');
+      $table->string('name')->comment('利用者名');
+      $table->integer('age')->comment('年齢');
+      $table->integer('sex')->comment('性別');
+      $table->string('diagnosis')->comment('診断名');
+      $table->text('note')->nullable()->comment('備考');
+      $table->softDeletes()->comment('削除日時');
+      $table->timestamp('created_at')->nullable()->comment('登録日時');
+      $table->timestamp('updated_at')->nullable()->comment('更新日時');
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('users');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('users');
+  }
 }
